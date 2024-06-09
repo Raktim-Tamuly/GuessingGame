11e482f9e2e59a2d2a22915ad35b317f79a5458b
raktimTamuly <168997324+Raktim-Tamuly@users.noreply.github.com>
2024-06-09 13:15:30 +0530

    index.js

---
+++ index.js
@@ -0,0 +1,21 @@
+const randomNo = parseInt(Math.random() * 100 + 1);
+const submit = document.querySelector("#sbmt");
+const userInput = document.querySelector("#guessField");
+const guessSlot = document.querySelector(".guesses");
+const remaining = document.querySelector(".lowOrHi");
+const startOver=document.querySelector(".resultParas")
+
+const p = document.querySelector("p");
+
+let prevGuess = [];
+let nummGuess = 1;
+let playGame = true;
+
+
+const validGuess = (guess) => {
+    //
+}
+
+const checkGuess = (guess) => {
+    
+}
